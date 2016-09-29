04c90d874a7f8ee8840d57dbff5897296a70dbbb
Blaz Pocrnja <blaz@ualberta.ca>
2016-09-29 17:40:30 -0600

    Finished diff function

--- mem_mod1.c
+++ mem_mod1.c
@@ -1,6 +1,7 @@
 #include "memlayout.h"
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int PAGE_SIZE = 4096;
 jmp_buf env;
@@ -20,17 +21,39 @@ int main(void){
 	sigaction(SIGSEGV, &sigsegv_act, NULL);
 
 	struct memregion regions[100]; 
+	struct memregion diffs[100];
+	int diffSize = sizeof(diffs)/sizeof(diffs[0]);
 	int arraySize = sizeof(regions)/sizeof(regions[0]);
 	int actualSize;
+	int actualDiffSize;
+	int i;
+
 	actualSize = get_mem_layout(&regions[0], arraySize);
 	
-	int i;
 	for(i = 0; i < actualSize; ++i){
 		printf("\n%-10p - %-10p %d \n", regions[i].from, regions[i].to, regions[i].mode);
 	}
 
 	printf("\n%d\n",actualSize);
+
+	int *test;
+	test = (int*) calloc(1000, sizeof(int));
 	
+	actualDiffSize = get_mem_diff(regions, arraySize, diffs, diffSize);
+
+	for(i = 0; i < actualDiffSize; ++i){
+		printf("\n%-10p - %-10p %d \n", diffs[i].from, diffs[i].to, diffs[i].mode);
+	}
+
+	printf("\n%d\n",actualDiffSize);
+
+	actualSize = get_mem_layout(&regions[0], arraySize);
+	
+	for(i = 0; i < actualSize; ++i){
+		printf("\n%-10p - %-10p %d \n", regions[i].from, regions[i].to, regions[i].mode);
+	}
+
+	printf("\n%d\n",actualSize);
 
 	
 	
--- memlayout.c
+++ memlayout.c
@@ -87,8 +87,8 @@ int get_mem_layout (struct memregion *regions, unsigned int size){
 		if(currentPageMode != lastPageMode){
 			//If we're still within the array
 			if(actualSize <= size){
-				//The last page was the final page of the previous memregion
-				regions[actualSize - 1].to = address - PAGE_SIZE;
+				//The last address location was the final location of the previous memregion
+				regions[actualSize - 1].to = address - 1;
 
 				//Add data to the new region
 				regions[actualSize].mode = currentPageMode;
@@ -104,5 +104,122 @@ int get_mem_layout (struct memregion *regions, unsigned int size){
 
 int get_mem_diff (struct memregion *regions, unsigned int howmany,
 struct memregion *thediff, unsigned int diffsize){
-	return 0;
+		
+	int actualDiffSize = 0;
+	unsigned int i = 0; 
+	unsigned int j = 0;
+	int jmpVar = 0;
+	char *address = (char*)0;
+	char data;
+
+	enum tryMode{
+		reading,
+		writing	
+	};
+
+	enum tryMode tryingMode = reading;
+	unsigned char oldCurrentPageMode, oldLastPageMode, currentPageMode, lastPageMode;
+
+	//Determine accessibility of location 0
+	jmpVar = sigsetjmp(env, 1);
+	if(jmpVar == 0){	
+		//Check if we can read
+		tryingMode = reading;
+		data = *address;
+		
+		//Check if we can write
+		tryingMode = writing;
+		*address = data;
+
+		//Writing successful
+		currentPageMode = MEM_RW;
+		
+	}
+	else{
+		//Something failed
+
+		if(tryingMode == writing){
+			//It failed trying to write but suceeded reading
+			currentPageMode = MEM_RO;
+
+		}
+		else{
+			//It failed trying to read
+			currentPageMode = MEM_NO;
+		}
+
+		
+	}
+	
+	oldCurrentPageMode = regions[0].mode;
+	
+	if(currentPageMode != oldCurrentPageMode){
+		if(actualDiffSize <= diffsize){
+			thediff[0].from = 0;
+			thediff[0].mode = currentPageMode;
+		}
+		++actualDiffSize;
+	}
+
+
+	for(i = 1; i <= 0xffffffff/PAGE_SIZE; ++i){
+		address = (char*)(long)(i*PAGE_SIZE);
+		lastPageMode = currentPageMode;
+		oldLastPageMode = oldCurrentPageMode;
+
+		//Determine accessibility of the page
+		jmpVar = sigsetjmp(env, 1);
+		if(jmpVar == 0){	
+			//Check if we can read
+			tryingMode = reading;
+			data = *address;
+			
+			//Check if we can write
+			tryingMode = writing;
+			*address = data;
+
+			//Writing successful
+			currentPageMode = MEM_RW;	
+		}
+		else{
+			//Something failed
+
+			if(tryingMode == writing){
+				//It failed trying to write but suceeded reading
+				currentPageMode = MEM_RO;
+			}
+			else{
+				//It failed trying to read
+				currentPageMode = MEM_NO;
+			}
+			
+		}
+		
+		//Get mode from old region you're in by checking all .to addresses
+		for(j = 0; j < howmany; ++j){
+			if(address <= (char*)regions[j].to){
+				oldCurrentPageMode = regions[j].mode;
+				break;
+			}
+		}
+		
+		//Checks all modes new and old and decides whether to add a starting address or an ending address to a diff region
+		if(lastPageMode != currentPageMode){		
+			if(lastPageMode != oldLastPageMode){
+				if(actualDiffSize <= diffsize){
+					thediff[actualDiffSize-1].to = address-1;
+				}	
+			}
+			if(currentPageMode != oldCurrentPageMode){
+				if(actualDiffSize <= diffsize){
+					thediff[actualDiffSize].from = address;
+					thediff[actualDiffSize].mode = currentPageMode;
+				}
+				++actualDiffSize;
+			}
+		}
+	}
+	
+	return actualDiffSize;
+
 }
